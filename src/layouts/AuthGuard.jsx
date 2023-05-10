406e083296a30889b3c68d6bfddfa5148575b833
Ahmed <aw708596@gmail.com>
2023-05-10 18:11:01 +0500

    completed

--- src/layouts/AuthGuard.jsx
+++ src/layouts/AuthGuard.jsx
@@ -9,6 +9,7 @@ import { decodeToken } from "react-jwt";
 const AuthGuard = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate()
+  const [loading,setLoading] = useState(true);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const { state: locationState, pathname } = useLocation();
 
@@ -26,15 +27,22 @@ const AuthGuard = () => {
     }else if(!token){
       dispatch(setLoggedIn(false))
     }
+    setLoading(false)
   }, []);
 
   useEffect(() => {
+    if(!loading){
       if(isLoggedIn){
         navigate(pathname)
       }else{
         navigate('/login')
       }
-  }, [isLoggedIn]);
+    }
+  }, [isLoggedIn, loading]);
+
+  if(loading){
+    return <div>Loading...</div>
+  }
 
   if (!isLoggedIn) {
     return <Navigate to="/login" state={{ visitedLocation: locationState }} />;
